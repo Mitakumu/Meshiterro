b5a2c0a65418413ef77279e573d35f324f447f58
Mitakumu <91771504+Mitakumu@users.noreply.github.com>
2021-11-13 13:30:35 +0900

    [Finish]

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'ff6abec79dc417549d9b91d115e15b9c1b7c98b73d4db5aa3e1152d33312d55a29c9dac5e4f698221defdd2f94a081dd1842bef839ac1828f8e96e890e151534'
